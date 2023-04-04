c59183b1c77568226dbbbb3f7a25c14237582afc
dylanquaale <dylanquaale33@gmail.com>
2023-04-04 18:34:44 -0500

    templating

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,59 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
-import NotFound from './pages/NotFound';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from "@apollo/client";
+import {setContext} from "@apollo/client/link/context";
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({
+  uri: "/graphql"
+})
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem("id_token")
+  return {
+    headers:{
+      ...headers,
+      authorization:token ? `bearer ${token}` : ''
+    }
+  }
+})
 
 const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
-          <Routes>
-            <Route 
-              path="/" 
-              element={<Home />}
-            />
-            <Route 
-              path="/matchup" 
-              element={<Matchup />}
-            />
-            <Route 
-              path="/matchup/:id" 
-              element={<Vote />}
-            />
-            <Route 
-              path="*"
-              element={<NotFound />}
-            />
-          </Routes>
-        </div>
-      </Router>
+
+    <ApolloProvider client= {client}>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route 
+            path='/' 
+            element={<SearchBooks />} 
+          />
+          <Route 
+            path='/saved' 
+            element={<SavedBooks />} 
+          />
+          <Route 
+            path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
+          />
+        </Routes>
+      </>
+    </Router>
     </ApolloProvider>
   );
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,27 +1,42 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
+// ！means that the field is non-nullable.
 const typeDefs = gql`
-  type Tech {
+  type User {
     _id: ID!
-    name: String!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
   }
-
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
   }
-
+  type Auth {
+    token: ID!
+    user: User
+  }
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    me: User
   }
-
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput): User
+    removeBook(bookId: ID!): User
   }
 `;
 
