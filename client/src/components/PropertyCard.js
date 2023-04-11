a82d5d2d1bcd18f7165d07a6b3ebabd5400129ed
NicholasHM05 <nhmamberger@gmail.com>
2023-04-10 21:11:22 -0500

    yay the homes are in a container

--- client/src/components/PropertyCard.js
+++ client/src/components/PropertyCard.js
@@ -1,5 +1,8 @@
+//idk how to refactor this in with the new code
+
+
+
 import * as React from 'react';
-// import axios from 'axios';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
@@ -15,14 +18,17 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-
-//idk how to refactor this in with the new code
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+const { QUERY_PROPERTIES } = require('../utils/queries');
+const { useQuery } = require('@apollo/client');
 
 // const PEXELS_API_KEY = 'dTxSFdIsdAJk7qP6gPugs1z5ftld7yXjc9AoOiQVScGERh4YmLl3skpP';
 
 // export default function SimpleContainer() {
 //     const [images, setImages] = React.useState([]);
-  
+
 //     React.useEffect(() => {
 //       const fetchImages = async () => {
 //         try {
@@ -35,33 +41,31 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 //               per_page: 6,
 //             },
 //           });
-  
+
 //           setImages(response.data.photos);
 //         } catch (error) {
 //           console.error('Error fetching images:', error);
 //         }
 //       };
-  
+
 //       fetchImages();
 //     }, []);
-  
-  //   return (
-  //     <React.Fragment>
-  //       <CssBaseline />
-  //       <Container maxWidth="lg">
-  //         <Grid container spacing={3}>
-  //           {images.map((image, index) => (
-  //             <Grid key={index} item xs={12} sm={6} md={4}>
-  //               <HomeReviewCardUno imageUrl={image.src.medium} />
-  //             </Grid>
-  //           ))}
-  //         </Grid>
-  //       </Container>
-  //     </React.Fragment>
-  //   );
-  // }
-const { QUERY_PROPERTIES } = require('../utils/queries');
-const { useQuery } = require('@apollo/client');
+
+//   return (
+//     <React.Fragment>
+//       <CssBaseline />
+//       <Container maxWidth="lg">
+//         <Grid container spacing={3}>
+//           {images.map((image, index) => (
+//             <Grid key={index} item xs={12} sm={6} md={4}>
+//               <HomeReviewCardUno imageUrl={image.src.medium} />
+//             </Grid>
+//           ))}
+//         </Grid>
+//       </Container>
+//     </React.Fragment>
+//   );
+// }
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -73,91 +77,91 @@ const ExpandMore = styled((props) => {
     duration: theme.transitions.duration.shortest,
   }),
 }));
-
 export default function PropertyCard() {
   const { loading, data } = useQuery(QUERY_PROPERTIES);
-
   const propertylist = data?.properties || [];
-
   console.log(propertylist);
-  
   const [expanded, setExpanded] = React.useState(false);
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-  
   return (
-    <div>
-      {propertylist.map((property, i) => (
-      <Card key={i} sx={{ maxWidth: 345 }}>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="Home">
-              H
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title={property.address}
-        />
-        <CardMedia
-          component="img"
-          height="194"
-          image={property.image}
-          alt="faker.address.streetAddress()"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </ExpandMore>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph>Property Info:</Typography>
-            <Typography paragraph>
-            {property.address}
-            <br />
-            {property.city}, 
-            {property.state}
-            <br />
-            {property.zipcode}
-            <br />
-            Price: ${property.price}
-            <br />
-            Bedrooms: {property.bedrooms}
-            <br />
-            Bathrooms: {property.bathrooms}
-            <br />
-            Sqft: {property.squareFeet}
-            </Typography>
-            <Typography paragraph>
-              
-            </Typography>
-            <Typography>
-              Additional Property Description
-            </Typography>
-          </CardContent>
-        </Collapse>
-      </Card>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          {propertylist.map((property, i) => (
+            <Grid key={i} item xs={12} sm={6} md={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardHeader
+                  avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="Home">
+                      H
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton aria-label="settings">
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title={property.address}
+                />
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image={property.image}
+                  alt="faker.address.streetAddress()"
+                />
+                <CardContent>
+                  <Typography variant="body2" color="text.secondary"></Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
+                  <IconButton aria-label="share">
+                    <ShareIcon />
+                  </IconButton>
+                  <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                  >
+                    <ExpandMoreIcon />
+                  </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                  <CardContent>
+                    <Typography paragraph>Property Info:</Typography>
+                    <Typography paragraph>
+                      {property.address}
+                      <br />
+                      {property.city},
+                      {property.state}
+                      <br />
+                      {property.zipcode}
+                      <br />
+                      Price: ${property.price}
+                      <br />
+                      Bedrooms: {property.bedrooms}
+                      <br />
+                      Bathrooms: {property.bathrooms}
+                      <br />
+                      Sqft: {property.squareFeet}
+                    </Typography>
+                    <Typography paragraph>
+                    </Typography>
+                    <Typography>
+                      Additional Property Description
+                    </Typography>
+                  </CardContent>
+                </Collapse>
+              </Card>
+            </Grid>
       ))}
-    </div>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
