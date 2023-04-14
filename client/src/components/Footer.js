4bca031ac26ae548c676faa70bf5cc392778db56
NicholasHM05 <nhmamberger@gmail.com>
2023-04-14 18:47:12 -0500

    added footer works for most part, some fixing to miguels code will make it fully work

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
 import logo from '../Assets/logo.jpg';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-// import { Container } from 'react-bootstrap';
-
-// const FootContainer = styled(Grid)({
-//   display: 'flex',
-//   flexDirection: 'column',
-//   minHeight: '100vh',
-// });
-
-// const Foot = styled(Grid)({
-//   flexGrow: '1',
-// });
+import { Container } from '@mui/material';
 
 const FooterText = styled(Typography)(({ theme }) => ({
   fontFamily: 'Trebuchet MS',
@@ -23,9 +12,9 @@ const FooterText = styled(Typography)(({ theme }) => ({
 }));
 
 const FooterRoot = styled('footer')(({ theme }) => ({
-  marginTop: '67%',
+  marginTop: '20px',
   backgroundColor: theme.palette.primary.dark,
-  padding: theme.spacing(6, 0),
+  padding: theme.spacing(2, 0),
 }));
 
 const FooterLink = styled(Link)(({ theme }) => ({
@@ -47,8 +36,8 @@ const LogoContainer = styled('div')({
 });
 
 const LogoImage = styled('img')(({ theme }) => ({
-  padding: '5px',
-  borderRadius: '50px',
+  padding: '35px',
+  borderRadius: '150px',
   width: 95,
   marginRight: theme.spacing(1),
 }));
@@ -56,40 +45,37 @@ const LogoImage = styled('img')(({ theme }) => ({
 export default function Footer() {
   return (
     <FooterRoot>
-      {/* <FootContainer>
-        <Foot> */}
-          <Grid container>
-            <Grid item xs={12} sm={6}>
-              <LogoContainer>
-                <LogoImage
-                  img src={logo}
-                  alt="HomeBros Logo"
-                />
-                <Typography variant="body1" color="textSecondary">
-                  <FooterText>&copy; HomeBros Real Estate 2023</FooterText>
-
-                </Typography>
-              </LogoContainer>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body1" color="textSecondary" align="right">
-                <FooterLink href="#">
-                  About Us
-                </FooterLink>
-                <FooterLink href="#">
-                  Contact Us
-                </FooterLink>
-                <FooterLink href="#">
-                  Privacy Policy
-                </FooterLink>
-                <FooterLink href="#">
-                  Terms &amp; Conditions
-                </FooterLink>
+      <Container>
+        <Grid container>
+          <Grid item xs={12} sm={6}>
+            <LogoContainer>
+              <LogoImage
+                img src={logo}
+                alt="HomeBros Logo"
+              />
+              <Typography variant="body1" color="textSecondary">
+                <FooterText>&copy; HomeBros Real Estate 2023</FooterText>
               </Typography>
-            </Grid>
+            </LogoContainer>
           </Grid>
-        {/* </Foot>
-      </FootContainer> */}
+          <Grid item xs={12} sm={6}>
+            <Typography variant="body1" color="textSecondary" align="right">
+              <FooterLink href="#">
+                About Us
+              </FooterLink>
+              <FooterLink href="#">
+                Contact Us
+              </FooterLink>
+              <FooterLink href="#">
+                Privacy Policy
+              </FooterLink>
+              <FooterLink href="#">
+                Terms &amp; Conditions
+              </FooterLink>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
     </FooterRoot>
   );
 }
