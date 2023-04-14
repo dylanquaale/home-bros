6481b9aafa57de3603e610a1da8416412e4f6bf9
kristermyr <110065764+kristermyr@users.noreply.github.com>
2023-04-14 11:35:58 -0600

    fixed error while running locally by putting old localhost link in, must put the mongooAtlas link  back when deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb+srv://dylanquaale:Atlas33@cluster0.qyjaiep.mongodb.net/fakerproperty_db?retryWrites=true&w=majority;",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/fakerproperty_db",
+  // FOR HEROKU: "mongodb+srv://dylanquaale:Atlas33@cluster0.qyjaiep.mongodb.net/fakerproperty_db?retryWrites=true&w=majority"
+  // FOR LOCAL:  'mongodb://localhost:27017/fakerproperty_db',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
