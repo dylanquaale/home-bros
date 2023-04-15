c0e093db0b425b95f167a96d031c08c504854561
NicholasHM05 <nhmamberger@gmail.com>
2023-04-14 22:13:33 -0500

    fixed connection issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/fakerproperty_db",
-  // FOR HEROKU: "mongodb+srv://dylanquaale:Atlas33@cluster0.qyjaiep.mongodb.net/fakerproperty_db?retryWrites=true&w=majority"
-  // FOR LOCAL:  'mongodb://localhost:27017/fakerproperty_db',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
+// This is our Heroku connection string
+const MONGODB_HEROKU = "mongodb+srv://dylanquaale:Atlas33@cluster0.qyjaiep.mongodb.net/fakerproperty_db?retryWrites=true&w=majority";
+// Our local connection string
+const MONGODB_LOCAL = "mongodb://localhost:27017/fakerproperty_db";
+// First, check if the MONGODB_URI environment variable is set
+const MONGODB_URI = process.env.MONGODB_URI;
+// If MONGODB_URI is set, use it, otherwise, use the Heroku connection string as a fallback for the site, and if that's not set, use the local connection string for the site instead
+const connectionString = MONGODB_URI || MONGODB_HEROKU || MONGODB_LOCAL;
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 module.exports = mongoose.connection;
