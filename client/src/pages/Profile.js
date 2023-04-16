71b62d5a08dcd813cfde4c75b2f90864de1d0b51
Dylan Quaale <93456860+dylanquaale@users.noreply.github.com>
2023-04-16 17:32:26 -0500

    Update Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,7 +3,7 @@ import UpdateUsername from '../components/UpdateUsername';
 import UpdateEmail from '../components/UpdateEmail';
 import UpdatePassword from '../components/UpdatePassword';
 import DisplayUserData from '../components/DisplayUserData';
-
+// component Profile displays the users data contaning all the updates
 const Profile = () => {
 
   return (
