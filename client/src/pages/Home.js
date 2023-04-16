6b946b2a393573a56725390e9c1ac91497272cc3
Dylan Quaale <93456860+dylanquaale@users.noreply.github.com>
2023-04-16 17:22:18 -0500

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,8 @@ import PropertyCard from '../components/PropertyCard';
 import { QUERY_ME } from '../utils/queries';
 import { useQuery } from '@apollo/client';
 
+// component named home uses the useQuery hook
+// property card is rendered displays the users saved properties 
 export default function Home() {
     const { loading, data } = useQuery(QUERY_ME);
     let userData = data?.me || [];
