2b333cd72a668a62ec44eaf80e85c072c94d77b6
Dylan Quaale <93456860+dylanquaale@users.noreply.github.com>
2023-04-16 17:11:07 -0500

    Update localStorage.js

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,3 +1,4 @@
+// exporting property ids from storage
 export const getSavedPropertyIds = () => {
     const savedPropertyIds = localStorage.getItem('saved_properties')
         ? JSON.parse(localStorage.getItem('saved_properties'))
@@ -5,7 +6,7 @@ export const getSavedPropertyIds = () => {
 
     return savedPropertyIds;
 };
-
+// save the property by id to local storage 
 export const savePropertyIds = (propertyIdArr) => {
     if (propertyIdArr.length) {
         localStorage.setItem('saved_properties', JSON.stringify(propertyIdArr));
@@ -13,18 +14,18 @@ export const savePropertyIds = (propertyIdArr) => {
         localStorage.removeItem('saved_properties');
     }
 };
-
+//remove the property from storage by the id
 export const removePropertyId = (propertyId) => {
     const savedPropertyIds = localStorage.getItem('saved_properties')
         ? JSON.parse(localStorage.getItem('saved_properties'))
         : null;
-
+// if false not there return false
     if (!savedPropertyIds) {
         return false;
     }
 
     const updatedSavedPropertyIds = savedPropertyIds?.filter((savedPropertyId) => savedPropertyId !== propertyId);
     localStorage.setItem('saved_properties', JSON.stringify(updatedSavedPropertyIds));
-
+// if true return true
     return true;
 };
